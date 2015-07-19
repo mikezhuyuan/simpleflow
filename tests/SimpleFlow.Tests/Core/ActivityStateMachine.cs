578b3c084125eccf32d2aeecabe58d557dd80c74
Mike Zhu <mzhu1@cpaglobal.com>
2015-07-19 22:05:02 +1000

    Unit test for activity state machine.

---
+++ tests/SimpleFlow.Tests/Core/ActivityStateMachine.cs
@@ -0,0 +1,29 @@
+﻿using NSubstitute;
+using SimpleFlow.Core;
+using Xunit;
+
+namespace SimpleFlow.Tests.Core
+{
+    public class ActivityStateMachineTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var repository = Substitute.For<IWorkItemRepository>();
+            var engine = Substitute.For<IEngine>();
+
+            var machine = new ActivityStateMachine(repository);
+
+            var workItem = new WorkItem
+            {
+                Id = Helpers.Integer(),
+                Status = WorkItemStatus.Created,
+            };
+
+            machine.Transit(workItem, engine);
+
+            repository.Received(1).Update(Arg.Is<WorkItem>(_ => _.Status == WorkItemStatus.Pending));
+            engine.Received(1).PostActivity(workItem.Id);
+        }
+    }
+}
