9faebbce4d08128e55d977db08d083d2d6b79fde
Mike Zhu <mzhu1@cpaglobal.com>
2015-07-21 23:34:23 +1000

    Added retry unit test.

---
+++ tests/SimpleFlow.Tests/Fluent/RetryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using SimpleFlow.Fluent;
+using Xunit;
+
+namespace SimpleFlow.Tests.Fluent
+{
+    public class RetryTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Func<string, int> func = int.Parse;
+
+            var a = FluentFlow.Activity(func).Retry().BuildBlock();
+
+            Assert.Equal(typeof(string), a.InputTypes.Single());
+            Assert.Equal(typeof(int), a.OutputType);
+
+            Assert.Equal("Retry(Activity)", a.ToString());
+        }
+    }
+}
